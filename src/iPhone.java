6c330822473e88ea05176238b616fc30e52e7aa3
Pedro Paulo de Almeida Marinho <135622492+Pedro-PAM@users.noreply.github.com>
2024-06-18 09:25:00 -0300

    Update iPhone.java

--- src/iPhone.java
+++ src/iPhone.java
@@ -1,1 +1,38 @@
+public class ReprodutorMusical {
+    public void exemploMetodo1() {
+        System.out.println("Exemplo do método exemploMetodo1 em ReprodutorMusical");
+    }
+
+    public void exemploMetodo2(String exemplo) {
+        System.out.println("Exemplo do método exemploMetodo2 em ReprodutorMusical com argumento: " + exemplo);
+    }
+}
+
+public class AparelhoTelefonico extends ReprodutorMusical {
+    @Override
+    public void exemploMetodo1() {
+        System.out.println("Exemplo do método exemploMetodo1 em AparelhoTelefonico");
+    }
+
+    @Override
+    public void exemploMetodo2(String exemplo) {
+        System.out.println("Exemplo do método exemploMetodo2 em AparelhoTelefonico com argumento: " + exemplo);
+    }
+}
+
+public class NavegadorInternet extends ReprodutorMusical {
+    @Override
+    public void exemploMetodo1() {
+        System.out.println("Exemplo do método exemploMetodo1 em NavegadorInternet");
+    }
+
+    @Override
+    public void exemploMetodo2(String exemplo) {
+        System.out.println("Exemplo do método exemploMetodo2 em NavegadorInternet com argumento: " + exemplo);
+    }
+}
+
+public class iPhone extends AparelhoTelefonico {
+    // Poderiam ser adicionados métodos específicos para o iPhone, se necessário
+}
 
